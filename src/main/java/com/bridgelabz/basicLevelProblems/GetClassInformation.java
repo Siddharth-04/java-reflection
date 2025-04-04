14ab5cedfd71f9dcc57738757f8927dd089bbee4
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-04 14:53:31 +0530

    [Siddharth] ADD: Get Class Information

---
+++ src/main/java/com/bridgelabz/basicLevelProblems/GetClassInformation.java
@@ -0,0 +1,83 @@
+package com.bridgelabz.basicLevelProblems;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+class Company{
+    private final String companyName;
+    String departmentName;
+    int departmentID;
+    int employees;
+
+    Company(String companyName, String departmentName, int departmentID, int employees) {
+        this.companyName = companyName;
+        this.departmentName = departmentName;
+        this.departmentID = departmentID;
+        this.employees = employees;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public int getDepartmentID() {
+        return departmentID;
+    }
+
+    public void setDepartmentID(int departmentID) {
+        this.departmentID = departmentID;
+    }
+
+    public int getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(int employees) {
+        this.employees = employees;
+    }
+}
+public class GetClassInformation {
+    public static void main(String[] args) {
+        Company company = new Company("Bridge LabZ", "Trainer", 2, 1);
+
+        Class obj = company.getClass();
+
+        //Method display
+        Method [] methods = obj.getDeclaredMethods();
+
+        System.out.println("Method Details \n");
+        for(Method method : methods) {
+            int modifiers = method.getModifiers();
+            String modifier = Modifier.toString(modifiers);
+            System.out.println(method.getName() + " , " + method.getReturnType() + " , " + modifier);
+        }
+
+        //Fields display
+        Field [] field =  obj.getDeclaredFields();
+        System.out.println("\nField Details \n");
+
+        for(Field f : field) {
+            int modifiers = f.getModifiers();
+            String modifier = Modifier.toString(modifiers);
+            System.out.println(f.getName() + "," + f.getType());
+        }
+
+        Constructor [] constructor = obj.getDeclaredConstructors();
+
+        System.out.println("\nConstructor Details \n");
+
+        for(Constructor c : constructor) {
+            System.out.println(c.getName());
+        }
+    }
+}
