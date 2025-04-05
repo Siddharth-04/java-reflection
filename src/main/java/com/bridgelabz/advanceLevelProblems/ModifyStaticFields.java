a1ce5d87969d1611b8a800dc690456924ffae412
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-05 13:42:15 +0530

    [Siddharth] ADD: Modify Static fields using reflection

---
+++ src/main/java/com/bridgelabz/advanceLevelProblems/ModifyStaticFields.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.advanceLevelProblems;
+
+import java.lang.reflect.Field;
+
+class Configuration{
+    private static String API_KEY = "apiKey";
+}
+public class ModifyStaticFields {
+    public static void modifyApiKey() throws Exception{
+        Class c = Configuration.class;
+
+        Field f = c.getDeclaredField("API_KEY");
+        f.setAccessible(true);
+        f.set(Configuration.class,"apiKeyChanged");
+        Object apiKey = f.get(Configuration.class);
+        System.out.println(apiKey);
+    }
+
+    public static void main(String[] args) {
+        try{
+            modifyApiKey();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
+//apiKeyChanged
